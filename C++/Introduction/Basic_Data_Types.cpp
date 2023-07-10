0ba7c17173a4d9e173f3e604a795bf21422e5b84
Athul Sankar K <103319795+Athul-sk@users.noreply.github.com>
2023-07-10 19:06:42 +0530

    Create Basic_Data_Types.cpp

---
+++ C++/Introduction/Basic_Data_Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>      // Including c standard input/output 
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf",&a ,&b , &c, &d, &e);      //This is a c input function. This can be done using c++ input function cin, but the code will be long
+    printf("%d\n%ld\n%c \n%f \n%lf",a ,b , c, d, e);    //This is a c output function. This can be done using c++ output function cout, but the code will be long
+
+    return 0;
+}
