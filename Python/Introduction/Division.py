c530bc460350cfd48e9bcb1d89e80a8123fa7af3
Athul Sankar K <103319795+Athul-sk@users.noreply.github.com>
2023-07-10 18:44:54 +0530

    Create Division.py

---
+++ Python/Introduction/Division.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)        # Prints integer division ( 7/2 = 3)
+    print(a/b)         # Prints float division ( 7/2 = 3.5)
