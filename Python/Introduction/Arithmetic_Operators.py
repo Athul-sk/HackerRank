bf1685b076b4c46fd9e8cb2bec4de75502c130ab
Athul Sankar K <103319795+Athul-sk@users.noreply.github.com>
2023-07-10 18:41:14 +0530

    Create Arithmetic_Operators.py

---
+++ Python/Introduction/Arithmetic_Operators.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)        # To print the sum of two numbers
+    print(a-b)        # To print the difference between two numbers
+    print(a*b)        # To print the product of two numbers 
